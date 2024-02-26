8303ed802d021df46c7e047ffca0f73afdc1b365
Mohamed Irfanullah M <mohamedirfan1720@gmail.com>
2024-02-27 03:16:25 +0530

    Middleware Added Successfully

---
+++ src/middlewares/file-upload.middleware.js
@@ -0,0 +1,13 @@
+import multer from "multer";
+
+const storageConfig = multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,"public/uploads");
+    },
+    filename:(req,file,cb)=>{
+        const name =file.originalname;
+        cb(null,name);
+    }
+}) 
+
+export const uploadFile = multer({storage:storageConfig});
